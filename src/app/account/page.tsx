e8e112e52ef6a5b4656a166bb6097b2f39cf9b95
Rudro Sarkar <souravkumar01986@gmail.com>
2026-09-16 23:37:25 +0600

    Update project

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,6 @@
 export default function AccountPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center bg-[#121212] text-white">
       <h1 className="text-2xl font-bold">Account Page</h1>
     </div>
   );
